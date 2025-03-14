45489230e0a7d59c67b18e489fd05db9c7609cf8
jbr-hill44 <jamesbooker.hill@gmail.com>
2025-03-14 19:34:02 +0000

    a few changes to get the app working

--- utils/db_handler.py
+++ utils/db_handler.py
@@ -24,45 +24,45 @@ engine = create_engine(DATABASE_URL)
 def verify_duplicate_user(email):
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
-    
+
     cursor.execute("SELECT COUNT(*) FROM users WHERE email = %s", (email,))
     count = cursor.fetchone()[0]
-    
+
     cursor.close()
     conn.close()
-    
+
     return count > 0
 
 
 def authenticate_user(email, password):
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
-    
+
     # Retrieve the stored hashed password for the user
     cursor.execute("SELECT password FROM users WHERE email = %s", (email,))
     stored_hashed_password = cursor.fetchone()
-    
+
     cursor.close()
     conn.close()
 
     # Check if a user with the given email was found
     if stored_hashed_password is None:
         return False
-    
+
     stored_hashed_password = stored_hashed_password[0]  # Extract the hash from the tuple
 
     # Compare the provided password with the stored hashed password
     return bcrypt.checkpw(password.encode(), stored_hashed_password.encode())
 
-    
+
 
 def save_user(email, password, role):
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
-    
+
     # Hash the password before saving
     hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode('utf-8')
-    
+
     # Base columns and values
     columns = ['email', 'password', 'role']
     values = [email, hashed_password, role]
@@ -71,46 +71,46 @@ def save_user(email, password, role):
     # for key in extra_input_params.keys():
     #     columns.append(key)
     #     values.append(st.session_state[f'{key}'])
-    
+
     # Dynamically build the SQL query
     columns_str = ', '.join(columns)
     placeholders = ', '.join(['%s'] * len(values))
-    
+
     query = sql.SQL("INSERT INTO users ({}) VALUES ({})").format(
         sql.SQL(columns_str),
         sql.SQL(placeholders)
     )
-    
+
     # Execute the query
     cursor.execute(query, values)
 
     conn.commit()
     cursor.close()
     conn.close()
-    
+
 
 def get_users():
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
-    
+
     cursor.execute("SELECT * FROM users")
     users = cursor.fetchall()
-    
+
     cursor.close()
     conn.close()
-    
+
     return users
 
 # def get_projects():
 #     conn = psycopg2.connect(**db_params)
 #     cursor = conn.cursor()
-    
+
 #     cursor.execute("SELECT * FROM projects")
 #     projects = cursor.fetchall()
-    
+
 #     cursor.close()
 #     conn.close()
-    
+
 #     return projects
 
 def get_projects():
